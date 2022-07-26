589b475c2e1747a0b92a36b4f6eb95e7b9b91bda
veerendraravella <Vveerendra.ravella@gmail.com>
2022-07-26 22:27:52 +0530

    adding the java doc

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        //Here we are printing the hllo world
         System.out.println("Hello world!");
     }
 }
